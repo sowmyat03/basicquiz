f6a070f2ed7e08e5f63e1a96ddc9e89ad8e26df9
sowmyaThota3 <sowmya@xam.com.au>
2025-01-12 15:39:19 +1100

    quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import questions from "./Question";
 
 function App() {
+  const [quesIndex, setQuesIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [answer, setAnswer] = useState("");
+
+  const handleClick = () => {
+    setQuesIndex(quesIndex + 1);
+    if (questions[quesIndex].answer === answer) setScore(score + 1);
+  };
+  const handleChange = (val: string) => {
+    setAnswer(val);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='App-header'>Quiz App</h1>
+
+      {quesIndex > 4 ? (
+        <>
+          <h2>Results</h2>
+          <p style={{ textAlign: "center" }}>Your Score is .... {score}</p>
+        </>
+      ) : (
+        <>
+          <p>{questions[quesIndex].question}</p>
+          {questions[quesIndex].options.map((option, i) => {
+            return (
+              <div>
+                <input
+                  type='radio'
+                  id={option}
+                  name='capital'
+                  onChange={(e) => {
+                    handleChange(e.target.value);
+                  }}
+                  checked={answer === option}
+                  value={option}
+                />
+                <label htmlFor={option}>{option}</label>
+              </div>
+            );
+          })}
+          <button onClick={handleClick}>Submit</button>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Question.ts
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is the capital of India?",
+    options: ["Seoul", "Tehran", "New Delhi", "Bangkok"],
+    answer: "New Delhi",
+  },
+  {
+    id: 2,
+    question: "What is the capital of Japan?",
+    options: ["Baghdad", "Tokyo", "Kabul", "Paris"],
+    answer: "Tokyo",
+  },
+  {
+    id: 3,
+    question: "What is the capital of Romania?",
+    options: ["Paris", "Athens", "Georgetown", "Bucharest"],
+    answer: "Bucharest",
+  },
+  {
+    id: 4,
+    question: "What is the capital of Indonesia?",
+    options: ["Banjul", "Jakarta", "Islamabad", "Yerevan"],
+    answer: "Jakarta",
+  },
+  {
+    id: 5,
+    question: "What is capital of Germany?",
+    options: ["Panama City", "Nicosia", "Berlin", "Georgetown"],
+    answer: "Berlin",
+  },
+];
+
+export default questions;
